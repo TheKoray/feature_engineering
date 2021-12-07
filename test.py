9efa28bcd4c6b522f22137550b07bb81f55701f5
TheKoray18 <korayldrm18@gmail.com>
2021-12-08 01:09:34 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 import numpy as np
 import pandas as pd
 
-from feature import feature
+#import create feature engineering module for missing 
+from feature import feature 
 
 data = pd.read_csv('https://www.openml.org/data/get_csv/16826755/phpMYEkMl')
 
@@ -14,8 +15,20 @@ import pandas as pd
 
 data = pd.read_csv('titanic.csv', usecols=['age','fare','survived'])
 
+
 ky = feature(data,'age')
 
-dagılım = ky.feature_dist(dagılım='Normal',distance=3, low_up='up')
+# mean imputation for missing_values
+ky.feature_mean()
 
-print(dagılım)
+# mode imputation for missing_values
+ky.feature_mod()
+
+# median imputation for missing_values
+ky.feature_median()
+
+# missing indicatory create new feature
+ky.missing_feature()
+
+#according to  feature distribution value impututation for missing values 
+ky.feature_dist(dagılım='normal',distance=3, low_up='up')
